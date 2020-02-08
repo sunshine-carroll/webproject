324157c7c64eab98a4a49e498a8e4ee03abca47f
sunshine <1959205721@qq.com>
2020-02-08 16:21:46 +0800

    sunshine

---
+++ api/src/main/java/com/sunshine/webproject/api/demo/DemoController.java
@@ -0,0 +1,9 @@
+package com.sunshine.webproject.api.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+}
